618a8a13bba29a38a5d77c74c71f5b4a01dc7c23
Chigozie Nwachukwu <ppaddy766@gmail.com>
2021-04-04 20:04:06 +0100

    Chapter Two

--- src/ChapterTwo/WorldPopulationGrowthCalculator.java
+++ src/ChapterTwo/WorldPopulationGrowthCalculator.java
@@ -1,20 +1,21 @@
 package ChapterTwo;
 
-import java.math.BigDecimal;
-
 public class WorldPopulationGrowthCalculator {
     public static void main(String[] args) {
-        long worldPopulation = 7-874-765-825;
-        long worldPopulation1 = (worldPopulation * (long) 0.1) + worldPopulation;
-        long worldPopulation2 = (worldPopulation1 * (long) 0.98 + worldPopulation1);
-        long worldPopulation3 = (worldPopulation2 * (long) 0.96 + worldPopulation2);
-        long worldPopulation4 = (worldPopulation3 * (long) 0.94 + worldPopulation3);
-        long worldPopulation5 = (worldPopulation4 * (long) 0.91 + worldPopulation4);
+        long worldPopulation = 7874765825L;
+        long worldPopulation1 = (long) ((worldPopulation * 0.1) + worldPopulation);
+        long worldPopulation2 = (long) ((worldPopulation1 * 0.98) + worldPopulation1);
+        long worldPopulation3 = (long) ((worldPopulation2 * 0.96) + worldPopulation2);
+        long worldPopulation4 = (long) ((worldPopulation3 * 0.94) + worldPopulation3);
+        long worldPopulation5 = (long) ((worldPopulation4 * 0.91) + worldPopulation4);
 
 
-        System.out.println ( "World Population Figures   " + "Year    " + " Estimated Growth Rate" );
-        System.out.println ( "7,874,765,825"  + " 2021\n" + worldPopulation1 + "2022 " + "0.1%\n" + worldPopulation2
-         + "2023" + "0.98%");
-        //System.out.println (  "2022" );
+        System.out.println ( "World_Population_Figures" + "\tYear    " + "\tEstimated Growth Rate" );
+        System.out.println (worldPopulation + "\t\t\t\t\t2021");
+        System.out.println (worldPopulation1 + "\t\t\t\t\t2022" + "\t\t\t\t\t0.1%");
+        System.out.println(worldPopulation2 + "\t\t\t\t\t2023" + "\t\t\t\t\t0.98%");
+        System.out.println (worldPopulation3 + "\t\t\t\t\t2022" + "\t\t\t\t\t0.1%");
+        System.out.println(worldPopulation4 + "\t\t\t\t\t2023" + "\t\t\t\t\t0.98%");
+        System.out.println (worldPopulation5 + "\t\t\t\t2022" + "\t\t\t\t\t0.1%");
     }
 }
